fda44a1c80882af9eb82f0aaff6eca3b46103e94
logisoltech <operations@logisol.tech>
2026-07-27 12:01:41 -0700

    Update HomePage.js

--- app/HomePage.js
+++ app/HomePage.js
@@ -82,7 +82,7 @@ export default function HomePage() {
               Environmental Infrastructure for ABA
             </p>
 
-            <h1 className="font-hero-georgia reveal-hero w-full text-[3.5rem] font-normal leading-[1.04] tracking-[-0.02em] text-black sm:text-7xl lg:text-[6.25rem] lg:leading-[1.01]">
+            <h1 className="font-hero-georgia reveal-hero w-full text-[3.75rem] font-normal leading-[1.04] tracking-[-0.06em] text-black sm:text-8xl lg:text-[7rem] lg:leading-[1.01]">
               Controlled Environments
               <br />
               For Growing ABA
